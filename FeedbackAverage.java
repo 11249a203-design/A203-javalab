890a7afc398b993f0db4efa88ebf72b3a32b3c4c
11249a203-design <11249a203@kanchiuniv.ac.in>
2026-09-11 12:23:57 +0530

    Add files via upload

---
+++ FeedbackAverage.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class FeedbackAverage {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+int[] ratings = new int[5];
+int sum = 0;
+ System.out.println("Enter 5 feedback ratings:");
+for (int i = 0; i < 5; i++) {
+System.out.print("Rating " + (i + 1) + ": ");
+ ratings[i] = sc.nextInt();
+sum = sum + ratings[i];
+ }
+double average = sum / 5.0;
+ System.out.println("Average Feedback Rating: " + average);
+sc.close();
+ }
+}
